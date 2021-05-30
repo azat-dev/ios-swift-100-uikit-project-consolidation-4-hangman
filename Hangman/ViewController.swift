db0b5341974fef1d81a08eca61715b73ad4d6979
Azat <kaumov.a.r@gmail.com>
2021-05-30 17:55:45 +0300

    Add game logic

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -11,11 +11,14 @@ class ViewController: UIViewController {
     
     var currentText: UITextField!
     var triesLabel: UILabel!
-
-    var round = 0
+    var roundLabel: UILabel!
+    
     var words = [String]()
     var word = ""
     var openedCharacters = [Character]()
+    
+    var round = -1
+    
     var tries = 0 {
         didSet {
             triesLabel.text = "Tries: \(tries)"
@@ -23,7 +26,13 @@ class ViewController: UIViewController {
     }
     
     func startNewRound() {
+        guard let randomWord = words.randomElement()?.lowercased() else {
+            return
+        }
         
+        round += 1
+        word = randomWord
+        currentText.text = getWordWithPlaceholders()
     }
     
     func loadWords() {
@@ -51,6 +60,7 @@ class ViewController: UIViewController {
         triesLabel = UILabel()
         triesLabel.translatesAutoresizingMaskIntoConstraints = false
         triesLabel.font = .systemFont(ofSize: 20)
+        triesLabel.textAlignment = .center
         tries = 0
         view.addSubview(triesLabel)
         
@@ -59,14 +69,16 @@ class ViewController: UIViewController {
         currentText.font = .systemFont(ofSize: 44)
         currentText.placeholder = "??????????"
         currentText.isUserInteractionEnabled = false
+        currentText.textAlignment = .center
         view.addSubview(currentText)
         
         let guessButton = UIButton(type: .system)
         guessButton.translatesAutoresizingMaskIntoConstraints = false
+        guessButton.titleLabel?.textAlignment = .center
         guessButton.setTitle("Guess the Letter", for: .normal)
         guessButton.addTarget(self, action: #selector(guessButtonTapped), for: .touchUpInside)
         view.addSubview(guessButton)
-
+        
         
         NSLayoutConstraint.activate([
             triesLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -102,16 +114,32 @@ class ViewController: UIViewController {
             }
         }
         
-        return text
+        return text.uppercased()
+    }
+    
+    func showSuccessAlert() {
+        let alert = UIAlertController(
+            title: "Success",
+            message: "Keep going!",
+            preferredStyle: .alert
+        )
+        
+        let continueButton = UIAlertAction(title: "Continue", style: .default) {
+            [weak self] _ in
+            self?.startNewRound()
+        }
+        
+        alert.addAction(continueButton)
+        present(alert, animated: true)
     }
     
     func submitCharacter(character: Character) {
-        guard openedCharacters.contains(character) else {
+        tries += 1
+        
+        guard !openedCharacters.contains(character) else {
             showErrorAlert(text: "The character \"\(character.uppercased())\" was opened before")
             return
         }
-        
-
         
         guard word.contains(character) else {
             showErrorAlert(text: "The word doesn't contain the character: \"\(character.uppercased())\"")
@@ -120,6 +148,15 @@ class ViewController: UIViewController {
         
         openedCharacters.append(character)
         currentText.text = getWordWithPlaceholders()
+        
+        let isWordCompleted = false
+        
+        if !isWordCompleted {
+            return
+        }
+        
+        words.removeAll { $0.lowercased() == word }
+        showSuccessAlert()
     }
     
     @objc func guessButtonTapped() {
@@ -145,15 +182,15 @@ class ViewController: UIViewController {
                 return
             }
             
-            self?.submitCharacter(character: text.first!.lowercased())
+            self?.submitCharacter(character: text.lowercased().first!)
         }
-
+        
         alert.addAction(.init(title: "Cancel", style: .cancel))
         alert.addAction(submitButton)
         
         present(alert, animated: true)
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
